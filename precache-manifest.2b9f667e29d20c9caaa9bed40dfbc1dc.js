ffd28d08db53010af42c6b8d941d7c5fd648c710
ZeroDawn0D <umang26112003@gmail.com>
2020-09-13 15:47:57 +0530

    init

---
+++ precache-manifest.2b9f667e29d20c9caaa9bed40dfbc1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6c1979f29d167d598868ac4c04dc10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "627953b9037d935521a2",
+    "url": "./static/css/main.1538f1bc.chunk.css"
+  },
+  {
+    "revision": "771c3f42aa48fe59d664",
+    "url": "./static/js/2.ff25d0c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ff25d0c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627953b9037d935521a2",
+    "url": "./static/js/main.fc0010c8.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
